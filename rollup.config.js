6a1e5600f7808cf1709e8ce29f31e7e2fd666905
Skayo <jonas@skayo.dev>
2020-11-09 15:27:11 +0100

    Dumped Sapper and use Svelte only

--- rollup.config.js
+++ rollup.config.js
@@ -1,138 +1,93 @@
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import url from '@rollup/plugin-url';
-import path from 'path';
 import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
 import sveltePreprocess from 'svelte-preprocess';
-import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const production = mode === 'production';
 
-const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning);
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true,
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		},
+	};
+}
 
 const preprocess = sveltePreprocess({
-	postcss: {
+	postcss:  {
 		plugins: [
 			require('postcss-import'),
 			require('tailwindcss'),
 			require('autoprefixer'),
-			...(process.env.NODE_ENV === 'production' ? [require('postcss-clean')] : []),
-		]
+			...(production ? [require('postcss-clean')] : []),
+		],
 	},
 	defaults: {
-		style: 'postcss'
-	}
+		style: 'postcss',
+	},
 });
 
 export default {
-	client: {
-		input:   config.client.input(),
-		output:  config.client.output(),
-		plugins: [
-			replace({
-				'process.browser':      true,
-				'process.env.NODE_ENV': JSON.stringify(mode),
-			}),
-			svelte({
-				dev,
-				hydratable: true,
-				emitCss: false,
-				preprocess
-			}),
-			url({
-				sourceDir:  path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/',
-			}),
-			resolve({
-				browser: true,
-				dedupe:  ['svelte'],
-			}),
-			commonjs(),
-
-			legacy && babel({
-				extensions:   ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude:      ['node_modules/@babel/**'],
-				presets:      [
-					[
-						'@babel/preset-env', {
-						targets: '> 0.25%, not dead',
-					},
-					],
-				],
-				plugins:      [
-					'@babel/plugin-syntax-dynamic-import',
-					[
-						'@babel/plugin-transform-runtime', {
-						useESModules: true,
-					},
-					],
-				],
-			}),
-
-			!dev && terser({
-				module: true,
-			}),
-		],
-
-		preserveEntrySignatures: false,
-		onwarn,
+	input:   'src/main.js',
+	output:  {
+		file:      'public/build/bundle.js',
+		sourcemap: !production,
+		format:    'iife',
+		name:      'app',
 	},
+	plugins: [
+		svelte({
+			// enable run-time checks when not in production
+			dev: !production,
+			// we'll extract any component CSS out into
+			// a separate file - better for performance
+			css: css => {
+				css.write('bundle.css', !production);
+			},
+			// preprocess svelte files
+			preprocess,
+		}),
 
-	server: {
-		input:    config.server.input(),
-		output:   config.server.output(),
-		plugins:  [
-			replace({
-				'process.browser':      false,
-				'process.env.NODE_ENV': JSON.stringify(mode),
-			}),
-			svelte({
-				generate:   'ssr',
-				hydratable: true,
-				dev,
-				emitCss: false,
-				preprocess
-			}),
-			url({
-				sourceDir:  path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/',
-				emitFiles:  false, // already emitted by client build
-			}),
-			resolve({
-				dedupe: ['svelte'],
-			}),
-			commonjs(),
-		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe:  ['svelte'],
+		}),
+		commonjs(),
 
-		preserveEntrySignatures: 'strict',
-		onwarn,
-	},
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
 
-	serviceworker: {
-		input:   config.serviceworker.input(),
-		output:  config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser':      true,
-				'process.env.NODE_ENV': JSON.stringify(mode),
-			}),
-			commonjs(),
-			!dev && terser(),
-		],
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 
-		preserveEntrySignatures: false,
-		onwarn,
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser(),
+	],
+	watch:   {
+		clearScreen: false,
 	},
 };
---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Board from './components/Board/index.svelte';
+	import Controls from './components/Controls/index.svelte';
+	import Header from './components/Header/index.svelte';
+	import Modal from './components/Modal/index.svelte';
+</script>
+
+<main>
+	<!-- Timer, Menu, etc. -->
+	<header>
+		<Header />
+	</header>
+
+	<!-- Sudoku Field -->
+	<section>
+		<Board />
+	</section>
+
+	<!-- Keyboard -->
+	<footer>
+		<Controls />
+	</footer>
+
+	<Modal />
+</main>
+
+<style global>
+	@import "./styles/global.css";
+
+	main {
+		@apply flex flex-col h-screen;
+	}
+
+	main > header {
+		@apply flex-grow;
+	}
+
+	main > footer {
+		@apply flex flex-grow flex-col justify-end;
+	}
+</style>
