a8e397471ca829c829799f0a899eb65fab97aa8a
Skayo <jonas@skayo.dev>
2020-11-10 00:48:13 +0100

    Improved PurgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,8 +28,21 @@ module.exports = {
   purge:    {
     content: ['./src/**/*.svelte', './src/**/*.html'],
     options: {
-      whitelistPatterns: [/^(row|col)-start-/],
-      //defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      whitelistPatterns: [/^(row|col)-start-/, /svelte-/],
+      defaultExtractor: (content) => {
+        const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+        const matchedTokens = [];
+        let match = regExp.exec(content);
+        while (match) {
+          if (match[0].startsWith('class:')) {
+            matchedTokens.push(match[0].substring(6));
+          } else {
+            matchedTokens.push(match[0]);
+          }
+          match = regExp.exec(content);
+        }
+        return matchedTokens;
+      },
     },
   },
   future:   {
