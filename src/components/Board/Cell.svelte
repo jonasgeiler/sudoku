07795db718c9920b11f05d350cbe42c374923b0e
Skayo <jonas@skayo.dev>
2020-11-10 22:57:24 +0100

    Validate cells

--- src/components/Board/Cell.svelte
+++ src/components/Board/Cell.svelte
@@ -12,15 +12,19 @@
 	// 0 when not running or empty
 	$: value = ($timerRunning && (grid.get($grid, cellX, cellY) || grid.get($userGrid, cellX, cellY))) || 0;
 
+	$: selected = cursor.isAt($cursor, cellX, cellY);
+
 	$: sameArea = $settings.highlightCells &&
-	              !cursor.isAt($cursor, cellX, cellY) &&
+	              !selected &&
 	              cursor.isInArea($cursor, cellX, cellY);
 
 	$: sameNumber = $settings.highlightSame &&
-	                !cursor.isAt($cursor, cellX, cellY) &&
 	                !cursor.isInitial($cursor) &&
+	                !selected &&
 	                value &&
 	                (grid.get($grid, $cursor.x, $cursor.y) || grid.get($userGrid, $cursor.x, $cursor.y)) === value;
+
+	$: conflictingNumber = $settings.highlightConflicting && !grid.isValid($grid, $userGrid, cellX, cellY, value);
 </script>
 
 <div class="cell row-start-{cellY} col-start-{cellX}"
@@ -32,9 +36,10 @@
 	{#if $timerRunning}
 		<div class="cell-inner"
 		     class:user-number={grid.isEmpty($grid, cellX, cellY)}
-		     class:selected={cursor.isAt($cursor, cellX, cellY)}
+		     class:selected={selected}
 		     class:same-area={sameArea}
-		     class:same-number={sameNumber}>
+		     class:same-number={sameNumber}
+		     class:conflicting-number={conflictingNumber}>
 
 			<button class="cell-btn" on:click={cursor.set(cellX, cellY)}>
 				<span class="cell-text">{value || ''}</span>
@@ -63,7 +68,7 @@
 	}
 
 	.cell-text {
-		@apply leading-full text-2xl;
+		@apply leading-full text-2xl transition-colors duration-100;
 	}
 
 	@screen sm {
@@ -86,5 +91,9 @@
 
 	.same-number {
 		@apply bg-primary-light;
+	}
+
+	.conflicting-number {
+		@apply text-red-600;
 	}
 </style>
